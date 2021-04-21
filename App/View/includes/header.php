b9e09c69f8af6269808cc1a9cafe3e58280b27e3
luisseidel <luis.guilherme.seidel@gmail.com>
2021-04-20 21:23:23 -0300

    #Created new side menu and css adjusts.

--- App/View/includes/header.php
+++ App/View/includes/header.php
@@ -10,48 +10,76 @@
     <meta name="robots" content="index,follow">
     <meta name="revisit-after" content="15 days" />
 
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" href="public/css/reset.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" href="public/css/style.css">
     <title>Document</title>
 </head>
-<body>
+<body class="flex-container">
 
-    <header class="container">
-        <nav class="mobile-menu">
-            <li class="haburger-menu">
-                <button onclick="openMenu()">
-                    <span class="material-icons md-24">face</span>
-                </button>
-            </li>
+    <header>
 
-            <li>
-                <h1>logo</h1>
-            </li>
-
-            <li></li>
-        </nav>
-
+        <div id="openMenu" class="openMenu">
+            <a onclick="openMenu()">
+                <span class="material-icons">menu</span>
+            </a>
+        </div>
         <nav id="sideNav" class="sidenav">
-            <li class="close-container">
-                <button onclick="closeMenu()">
-                    <i class="fas fa-times fa-lg"></i>
-                </button>
-            </li>
-            <li>
-                <a href="#">
-                    <i class="far fa-id-card"></i>
-                    <p>Profile</p>
-                </a>
-            </li>
+            <div class="sidenav-body sidenav-container">
+                <div class="menu-header">
+                    <a onclick="closeMenu()">
+                        <span class="material-icons">close</span>
+                    </a>
+                </div>
 
-            <li>
-                <a href="#">
-                    <i class="fas fa-briefcase"></i>
-                    <p>Porfolio</p>
-                </a>
-            </li>
+                <div class="menu-body">
+                    <div class="menu-item">
+                        <a class="menu-item-header" onclick="dropDown(this)">
+                            <span class="material-icons">create</span>
+                            <span>Cadastros</span>
+                            <span class="material-icons seta-down">expand_more</span>
+                        </a>
 
+                        <div class="menu-item-body close-item">
+                            <a href="App/View/pages/pessoas.php"><span class="material-icons">person</span>Pessoas</a>
+                            <a href="App/View/pages/pessoas.php"><span class="material-icons">group</span>Tipo Pessoa</a>
+                            <a href="App/View/pages/pessoas.php"><span class="material-icons">flag</span>Países</a>
+                            <a href="App/View/pages/pessoas.php"><span class="material-icons">location_city</span>Estados</a>
+                            <a href="App/View/pages/pessoas.php"><span class="material-icons">apartment</span>Cidades</a>
+                        </div>
+                    </div>
+
+
+                </div>
+            </div>
+
+            <div class="sidenav-footer sidenav-container">
+                <div class="social-icon">
+                    <a href="https://github.com/luisseidel" target="_blank">
+                        <img src="public/icons/github-white.svg">
+                    </a>
+                </div>
+                <div class="social-icon">
+                    <a href="https://api.whatsapp.com/send?phone=5551997772989&text=Ol%C3%A1%2C%20tudo%20bem%3F" target="_blank">
+                        <img src="public/icons/whatsapp-white.svg">
+                    </a>
+                </div>
+                <div class="social-icon">
+                    <a href="https://www.instagram.com/luisgseidel/" target="_blank">
+                        <img src="public/icons/instagram-white.svg">
+                    </a>
+                </div>
+                <div class="social-icon">
+                    <a href="https://www.linkedin.com/in/luis-seidel/" target="_blank">
+                        <img src="public/icons/linkedin-white.svg">
+                    </a>
+                </div>
+                <div class="social-icon">
+                    <a href="https://www.facebook.com/guilhermeseidel" target="_blank">
+                        <img src="public/icons/facebook-white.svg">
+                    </a>
+                </div>
+            </div>
         </nav>
 
     </header>
